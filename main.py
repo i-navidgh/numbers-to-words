26653dffd19405a0eaf78e44991aaa93125d55db
i-navidgh <36674431+i-navidgh@users.noreply.github.com>
2024-01-20 08:15:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from num2words import num2words
-
 def number_to_word(number: int):
     
+    units = ['یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه']
+    tens = ['ده', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود']
+    hundreds = ['صد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد']
+    digits = len(str(number))
+    array = [int(x) for x in str(number)]
+    string = ""
+    for i in range(digits):
+        if(i <= 2):
+            string += str(array[i])
+        elif(i <= 5):
+            pass
+    return string
 
 
 print(number_to_word(12345))
